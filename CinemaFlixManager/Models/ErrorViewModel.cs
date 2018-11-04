6cd80081b25ab954c259486c42d3e88978579e70
liusaibot <omotayosaliu@yahoo.com>
2018-11-04 15:29:32 +0100

    Add project files.

---
+++ CinemaFlixManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CinemaFlixManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
